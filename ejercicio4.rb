f2caff2938de2e6e193dbc060b24cc6d5620551f
paskuvan <majopaskuvan@gmail.com>
2018-06-13 08:08:25 -0400

    not resolved :sweat:

--- ejercicio4.rb
+++ ejercicio4.rb
@@ -1,4 +1,12 @@
 personas = ["Carolina", "Alejandro", "Maria Jesús", "Valentín"]
 edades = [32, 28, 41, 19]
 
+def personas(hash)
+	 edades = 0
+	 hash.each do |key,value|
+	 edades += value
 
+end
+
+	edades.each { |value| edades / 4 }
+end
