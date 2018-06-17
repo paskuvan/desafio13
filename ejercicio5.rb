73dfefadd9075459f5439f31a62f5e04085d8be0
paskuvan <majopaskuvan@gmail.com>
2018-06-16 23:17:44 -0400

    resolved

--- ejercicio5.rb
+++ ejercicio5.rb
@@ -1,2 +1,12 @@
 meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo']
 ventas = [2000, 3000, 1000, 5000, 4000]
+
+h = Hash[meses.zip(ventas)]
+
+print h, "\n"
+
+invertir_hash = h.invert
+
+valor_max = invertir_hash.max_by{|k,v| k}
+
+puts valor_max[1]
