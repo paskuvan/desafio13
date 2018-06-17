3d5a0f2c704205362ed009b866eb31ed6a47ea55
paskuvan <majopaskuvan@gmail.com>
2018-06-16 23:30:56 -0400

    solved :sunglasses:

--- ejercicio6.rb
+++ ejercicio6.rb
@@ -1,1 +1,23 @@
-restaurant_menu = { "Ramen" => 3, "Dal Makhani" => 4, "Coffee" => 2 }
+menu = { "Ramen" => 3, "Dal Makhani" => 4, "Coffee" => 2 }
+
+carisimo = menu.max_by{|k,v| v} #mostrar el mas caro
+puts carisimo[0]
+
+baratisimo = menu.min_by{|k,v| v} #mostrar el mas baratito
+puts baratisimo[0]
+suma = menu.values.inject(0){|sum, x| sum + x} #se suma el valor del plato
+
+puts suma / menu.size 
+
+platos = menu.keys #nombre de platos
+
+puts platos, "\n"
+
+precios = menu.values #mostrar el precio
+puts precios, "\n"
+
+menu.each {|k, v| menu[k] = v * 1.19} #19% de IVA
+puts menu, "\n"
+
+
+#la ultima pregunta no lo entiendo sorry
